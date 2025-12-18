662bd4a77c753b5f7bae3ab3b463b92d23815113
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-18 21:03:53 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -2,21 +2,15 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const adminSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-adminSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+adminSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 adminSchema.methods.matchPassword = async function (enteredPassword) {
