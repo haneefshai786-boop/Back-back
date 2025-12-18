b7cf2b70b551e25bd8608693c08156f63a640769
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-18 21:03:26 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,25 +2,13 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-// ✅ FIXED: no next(), just return
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
