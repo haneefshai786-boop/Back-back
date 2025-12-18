581238a7afdd369b881d6c2d753fe90d92dac225
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-18 21:14:22 +0530

    Update adminAuthRoutes.js

--- routes/adminAuthRoutes.js
+++ routes/adminAuthRoutes.js
@@ -1,8 +1,5 @@
 import express from "express";
 import { adminLogin } from "../controllers/adminAuthController.js";
-
 const router = express.Router();
-
 router.post("/login", adminLogin);
-
 export default router;
