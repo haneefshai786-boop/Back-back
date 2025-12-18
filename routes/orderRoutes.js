9f6d69cf33b7172798a8ee294ac0739adf4121c9
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-18 21:14:59 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createOrder, getUserOrders } from "../controllers/orderController.js";
-import { userAuth } from "../middleware/userAuth.js";
+import { createOrder, getUserOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { userAuth } from "../middleware/authMiddleware.js";
+import { adminAuth } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
+// User
 router.post("/", userAuth, createOrder);
 router.get("/my", userAuth, getUserOrders);
+
+// Admin
+router.get("/", adminAuth, getAllOrders);
+router.patch("/:id", adminAuth, updateOrderStatus);
 
 export default router;
