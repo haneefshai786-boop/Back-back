ab7a7eb62e39fe12dead249ad98ff54d6d45f01b
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 14:11:30 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  subCategory: { type: mongoose.Schema.Types.ObjectId, ref: "SubCategory", required: true },
-  image: String,
+  description: { type: String },
+  image: { type: String },
+  vendor: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
+  subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "SubCategory", required: true },
 });
 
 export default mongoose.model("Product", productSchema);
