fc1c4523a92382218b3b4cbde14f0949775aeb9f
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 11:32:52 +0530

    Update adminMiddleware.js

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,18 +1,31 @@
 import jwt from "jsonwebtoken";
 import Admin from "../models/Admin.js";
 
-export const adminAuth = async (req, res, next) => {
-  let token = req.headers.authorization;
-  if (!token || !token.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Not authorized" });
-  }
-  token = token.split(" ")[1];
-
+export const protectAdmin = async (req, res, next) => {
   try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Admin not authorized, no token" });
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.admin = await Admin.findById(decoded.id).select("-password");
+
+    if (!req.admin) {
+      return res.status(401).json({ message: "Admin not found" });
+    }
+
     next();
-  } catch (err) {
-    res.status(401).json({ message: "Token failed" });
+  } catch (error) {
+    res.status(401).json({ message: "Admin not authorized" });
   }
 };
