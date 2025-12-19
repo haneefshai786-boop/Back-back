6af35c31a517520f84d0bcfded470b8e7333a1d3
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 15:05:29 +0530

    Update subCategoryRoutes.js

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -1,23 +1,8 @@
 import express from "express";
-import {
-  createSubCategory,
-  updateSubCategory,
-  deleteSubCategory,
-  getSubCategories,
-  getProductsBySubCategory
-} from "../controllers/subCategoryController.js";
-
-import { protectAdmin } from "../middleware/adminMiddleware.js";
+import { getProductsBySubCategory } from "../controllers/productController.js";
 
 const router = express.Router();
 
-// Admin CRUD
-router.post("/", protectAdmin, createSubCategory);
-router.put("/:id", protectAdmin, updateSubCategory);
-router.delete("/:id", protectAdmin, deleteSubCategory);
-
-// User fetch
-router.get("/", getSubCategories);
 router.get("/:subCategoryId/products", getProductsBySubCategory);
 
 export default router;
