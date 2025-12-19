5dfe5eb16f5c8de370d458fc40fa6dd5f010231d
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 14:40:32 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { createProduct, getProducts } from "../controllers/productController.js";
+import {
+  createProduct,
+  getProducts,
+  getProductsBySubCategory
+} from "../controllers/productController.js";
+
 import { protectAdmin } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
+/* ADMIN */
 router.post("/", protectAdmin, createProduct);
+
+/* USER */
 router.get("/", getProducts);
+router.get("/subcategory/:id", getProductsBySubCategory);
 
 export default router;
