18834d95eb261292678388df5ea5e6f7ec6da941
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 11:50:52 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,17 +1,23 @@
 import express from "express";
 import {
   createVendor,
-  getVendorsByFolder
+  updateVendor,
+  deleteVendor,
+  getVendorsByFolder,
+  getCategoriesByVendor
 } from "../controllers/vendorController.js";
 
 import { protectAdmin } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
-// Public (user)
-router.get("/folder/:folderId", getVendorsByFolder);
-
-// Admin only
+// ADMIN CRUD
 router.post("/", protectAdmin, createVendor);
+router.put("/:id", protectAdmin, updateVendor);
+router.delete("/:id", protectAdmin, deleteVendor);
+
+// USER FLOW
+router.get("/folder/:folderId", getVendorsByFolder);
+router.get("/:vendorId/categories", getCategoriesByVendor);
 
 export default router;
