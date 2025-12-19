4352785658d58a97f7a0245515c6c114de8b340d
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-19 11:51:21 +0530

    Update vendorController.js

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,32 +1,38 @@
 import Vendor from "../models/Vendor.js";
 import Category from "../models/Category.js";
 
-// Create vendor
+// ✅ Create vendor (ADMIN)
 export const createVendor = async (req, res) => {
-  const vendor = await Vendor.create({ name: req.body.name, folder: req.body.folder });
+  const vendor = await Vendor.create(req.body);
   res.status(201).json(vendor);
 };
 
-// Update vendor
+// ✅ Update vendor (ADMIN)
 export const updateVendor = async (req, res) => {
-  const vendor = await Vendor.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+  const vendor = await Vendor.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
   res.json(vendor);
 };
 
-// Delete vendor
+// ✅ Delete vendor (ADMIN)
 export const deleteVendor = async (req, res) => {
-  const vendor = await Vendor.findByIdAndDelete(req.params.id);
-  res.json({ message: "Vendor deleted", vendor });
+  await Vendor.findByIdAndDelete(req.params.id);
+  res.json({ message: "Vendor deleted" });
 };
 
-// Get all vendors
-export const getVendors = async (req, res) => {
-  const vendors = await Vendor.find();
+// ✅ Get vendors by folder (USER)
+export const getVendorsByFolder = async (req, res) => {
+  const vendors = await Vendor.find({ folder: req.params.folderId });
   res.json(vendors);
 };
 
-// Get categories by vendor
+// ✅ Get categories of a vendor (USER)
 export const getCategoriesByVendor = async (req, res) => {
-  const categories = await Category.find({ vendor: req.params.vendorId });
+  const categories = await Category.find({
+    vendor: req.params.vendorId,
+  });
   res.json(categories);
 };
